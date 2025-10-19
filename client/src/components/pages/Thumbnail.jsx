116c5385fa73d5a59fb768d19d99051a9f207b8e
Ritesh Das <riteshdas8652@gmail.com>
2025-10-19 10:23:04 +0530

    frontend

---
+++ client/src/components/pages/Thumbnail.jsx
@@ -0,0 +1,12 @@
+import Navbar from '@/components/shared/Navbar'
+import React from 'react'
+
+const Thumbnail = () => {
+  return (
+    <div>
+      <Navbar/>
+    </div>
+  )
+}
+
+export default Thumbnail
