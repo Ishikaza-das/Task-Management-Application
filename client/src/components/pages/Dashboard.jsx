3c6d38d265f720c250c58d4e1cc638999e2e2489
Ritesh Das <riteshdas8652@gmail.com>
2025-10-24 10:25:00 +0530

    Login Signup

---
+++ client/src/components/pages/Dashboard.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from '../shared/Navbar'
+
+const Dashboard = () => {
+  return (
+    <div>
+        <Navbar/>
+        <div>
+            
+        </div>
+    </div>
+  )
+}
+
+export default Dashboard
